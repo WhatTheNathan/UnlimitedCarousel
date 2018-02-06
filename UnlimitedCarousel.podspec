47bd3bd399cc3df1fab4157292ecde585872a1c4
WhatTheNathan <nathanliuyolo@gmail.com>
2018-02-06 12:06:29 +0800

    version 1.2.3

--- UnlimitedCarousel.podspec
+++ UnlimitedCarousel.podspec
@@ -16,7 +16,7 @@ Pod::Spec.new do |s|
   #
 
   s.name         = "UnlimitedCarousel"
-  s.version      = "1.2.2"
+  s.version      = "1.2.3"
   s.summary      = "Highly customized Carousel Figure for iOS developed in Swift"
 
   s.license      = "BSD"
--- UnlimitedCarousel/UICollectionDelegate.swift
+++ UnlimitedCarousel/UICollectionDelegate.swift
@@ -11,15 +11,15 @@ import UIKit
 import SDWebImage
 
 extension UnlimitedCarousel: UICollectionViewDataSource {
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
+    public func numberOfSections(in collectionView: UICollectionView) -> Int {
         return numOfSection
     }
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return numOfFigures
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Figure", for: indexPath) as! FigureCell
         cell.image.contentMode = .scaleToFill
         if let dataSource = self.dataSource {
@@ -30,24 +30,24 @@ extension UnlimitedCarousel: UICollectionViewDataSource {
 }
 
 extension UnlimitedCarousel: UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    public func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         delegate?.UnlimitedCarousel(self, didSelectFigureAt: ICIndexPath.init(column: indexPath.section, row: indexPath.row))
     }
     
     /// Call when you manually drag, scroll to the middle section
-    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+    public func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
         guard let dataSource = self.dataSource else { return }
         collectionView.scrollToItem(at: IndexPath(item: pageControl.currentPage, section: dataSource.numberOfSections(in: self)/2), at: .left, animated: false)
     }
     
     /// Call when auto srcoll, scroll to the middle section
-    func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
+    public func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
         guard let dataSource = self.dataSource else { return }
         collectionView.scrollToItem(at: IndexPath(item: pageControl.currentPage, section: dataSource.numberOfSections(in: self)/2), at: .left, animated: false)
     }
     
     /// compute the current page
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+    public func scrollViewDidScroll(_ scrollView: UIScrollView) {
         guard let dataSource = self.dataSource else { return }
         let page = Int((scrollView.contentOffset.x + (collectionView.bounds.width) * 0.5) / (collectionView.bounds.width))
         let currentPage = page % dataSource.numberOfFigures(for: self)
@@ -55,12 +55,12 @@ extension UnlimitedCarousel: UICollectionViewDelegate {
     }
     
     /// stop auto scroll when you manually drag
-    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+    public func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
         endAutoScroll()
     }
     
     /// start auto scroll when you stop manuallt dragging
-    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+    public func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
         startAutoScroll()
     }
 }
